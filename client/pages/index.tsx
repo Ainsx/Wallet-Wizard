f34377f902e24878fdd44afa739073c2fd5d2ecc
Daekeun Kim <eorms9762@gmail.com>
2026-06-13 22:22:15 -0400

    adding client as regular directory

---
+++ client/pages/index.tsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+
+function index() {
+    const [message, setMessage] = useState("Loading...");
+
+    useEffect(() => {
+        fetch("http://localhost:3100/api/home")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setMessage(data.message);
+        });
+    }, []);
+  return (
+    <div>{message}</div>
+  )
+
+}
+
+export default index
+
