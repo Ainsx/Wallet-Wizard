192fb47dd4514885b246136d0f41ba809847b0a5
Daekeun Kim <eorms9762@gmail.com>
2026-06-17 15:08:39 -0400

    change test

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = 3100;
 app.use(cors());
 
 app.get("/api/home", (req, res) => {
-    res.json({ message: "Welcome to Wallet Wizard Project!"})
+    res.json({ message: "Welcome to Wallet Wizard Project!!"})
 });
 
 app.listen(PORT, () =>{
