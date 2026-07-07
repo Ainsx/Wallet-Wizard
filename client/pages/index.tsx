d90c0af89e0646f257fffa32a6934666a019b47a
Daekeun Kim <eorms9762@gmail.com>
2026-07-07 06:25:44 -0400

    feat: displays example transaction table

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,49 @@
 import React, {useEffect, useState} from 'react';
 
-function index() {
-    const [message, setMessage] = useState("Loading...");
+function Index() {
+    const [transaction, setTransaction] = useState<
+    {id: string; name: string; category_name: string;
+      date: Date; amount: number; created_at: Date}[] | string
+    >("loading");
 
-    useEffect(() => {
-        fetch("http://localhost:3100/api/home")
+    useEffect(() => { /*local host*/
+        fetch("http://localhost:3100/api/transaction")
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-            setMessage(data.message);
+            setTransaction(data);
         });
     }, []);
   return (
-    <div>{message}</div>
-  )
-
+    <div>
+      <table border = {2} align="center" cellPadding={15} cellSpacing={5}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Transaction Name</th>
+            <th>Category Name</th>
+            <th>Date</th>
+            <th>Amount</th>
+            <th>Created at</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(transaction) && transaction.map((item, index)=>(
+            <tr key={item.id}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.category_name}</td>
+              <td>{new Date(item.date).toLocaleDateString()}</td>
+              <td align="right">{(item.amount / 100).toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</td>
+              <td>{new Date(item.created_at).toLocaleString('en-US', {timeZoneName: 'short'})}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
+export default Index;
 
-export default index
+
 
